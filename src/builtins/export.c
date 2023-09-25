b9a9572bde2f3ec553c7137fec13bc45951873f5
Jordan42 <contact@jordan-chap.com>
2023-09-25 19:38:34 +0200

    nomore crash with export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: jchapell <jchapell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 17:30:15 by arthur            #+#    #+#             */
-/*   Updated: 2023/09/25 17:19:41 by jchapell         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:37:59 by jchapell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,12 +72,13 @@ void	add_env(char **arg, char ***env)
 	i = 0;
 	while ((*env)[i++])
 		;
-	new_env = malloc(sizeof(char *) * (i + 2));
+	new_env = malloc(sizeof(char *) * (i + 1));
 	i = -1;
 	while ((*env)[++i])
 		new_env[i] = ft_strdup((*env)[i]);
 	new_env[i] = add_str(arg[0], "=", 1);
 	new_env[i] = add_str(new_env[i], arg[1], 3);
+	free(*env);
 	*env = new_env;
 }
 
--- src/start.c
+++ src/start.c
@@ -6,22 +6,34 @@
 /*   By: jchapell <jchapell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 17:00:57 by jchapell          #+#    #+#             */
-/*   Updated: 2023/09/25 18:29:29 by jchapell         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:36:25 by jchapell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/proto.h"
 
+char	**create_envar(char *key, char *value)
+{
+	char **res;
+
+	res = malloc(sizeof(char *) * 3);
+	res[0] = ft_strdup(key);
+	res[1] = ft_strdup(value);
+	res[2] = NULL;
+	return (res);
+}
+
 void	create_envp(char ***envp)
 {
-	t_cmd	tmp, tmp2;
+	char **new_envp;
 
 	printf("Create env...\n");
-	tmp = create_cmd("", "SHLVL=0", 0);
-	ft_export(&tmp, envp);
-	// free_cmd(&tmp);
-	// tmp2 = create_cmd("", "PWD=0", 0);
-	// ft_export(&tmp2, envp);
+	new_envp = malloc(sizeof(char *) * 4);
+	new_envp[0] = ft_strdup("SHLVL=0");
+	new_envp[1] = ft_strdup("PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin");
+	new_envp[2] = add_str("PWD=", getcwd(NULL, 0), 0);
+	new_envp[3] = NULL;
+	*envp = new_envp;
 }
 
 void	start(int ac, char **av, char ***envp)
