2e3f2909a974075a75b7fba95c0f5e0f122be68c
Jordan Chapelle <lebojo@ov-55f615.infomaniak.ch>
2023-11-03 19:32:42 +0000

    omg l'echo

--- src/expander.c
+++ src/expander.c
@@ -6,7 +6,7 @@
 /*   By: lebojo <lebojo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 04:19:20 by jchapell          #+#    #+#             */
-/*   Updated: 2023/11/02 18:36:23 by lebojo           ###   ########.fr       */
+/*   Updated: 2023/11/03 19:31:32 by lebojo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,27 +37,13 @@ t_cmd	create_cmd(char *name, char *arg, int which_pipe, int fred)
 	return (res);
 }
 
-int	is_inquote(char *str, int i)
-{
-	int	res;
-	int	start;
-
-	res = -1;
-	start = i;
-	while (i >= 0 && res == -1)
-		if (str[i--] == '\'')
-			res++;
-	i = start;
-	while (str[++i] && res == 0)
-		if (str[i] == '\'')
-			res++;
-	return (res);
-}
-
-char	*char_to_str(char c)
+char	*char_to_str(char c, int q, int q2)
 {
 	char	*res;
 
+	if (((q == double_q || q2 == double_q) && c == '"')
+		|| ((q == simple || q2 == simple) && c == '\''))
+		return (ft_strdup(""));
 	res = malloc(sizeof(char) * 2);
 	res[0] = c;
 	res[1] = '\0';
@@ -69,12 +55,12 @@ char	*process_expand(char ***envp, char *src, char *res, t_inc *incr)
 	char	*tmp;
 	char	*var;
 
-	if (src[incr->i] == '$' && is_inquote(src, incr->i) != 1)
+	if (src[incr->i] == '$' && incr->q != simple)
 	{
-		tmp = ft_calloc(ft_strlen(src), sizeof(char));
-		while (src[++incr->i] && !char_in_str(src[incr->i], " \t\"$"))
+		tmp = ft_calloc(ft_strlen(src) + 1, sizeof(char));
+		while (src[++incr->i] && !char_in_str(src[incr->i], " \t\"'$"))
 			tmp[incr->k++] = src[incr->i];
-		var = hm_get_value((*envp), tmp);
+		var = hm_get_value(*envp, tmp);
 		if (var)
 		{
 			res = add_str(res, var, 3);
@@ -84,7 +70,7 @@ char	*process_expand(char ***envp, char *src, char *res, t_inc *incr)
 		incr->k = 0;
 	}
 	else
-		res = add_str(res, char_to_str(src[incr->i]), 3);
+		res = add_str(res, char_to_str(src[incr->i], incr->q, incr->r), 3);
 	return (res);
 }
 
@@ -103,13 +89,9 @@ char	*expand(char *src, char ***envp)
 		return (NULL);
 	while (src && src[++incr.i])
 	{
-		if (quote == none)
-			rev_quote(&quote, src[incr.i]);
-		if ((quote == double_q && src[incr.i] == '"')
-			|| (quote == simple && src[incr.i] == '\''))
-			continue ;
-		if (quote != none)
-			rev_quote(&quote, src[incr.i]);
+		incr.r = quote;
+		rev_quote(&quote, src[incr.i]);
+		incr.q = quote;
 		res = process_expand(envp, src, res, &incr);
 		if (!src[incr.i])
 			break ;
